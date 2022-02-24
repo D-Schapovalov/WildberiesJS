d6397139b738224877dd91d600d9f5c30848f82c
Dmitriy Shapovalov <shapovalov-84@internet.ru>
2022-02-24 21:57:00 +0700

    create:getGoods.js

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,22 @@
-const cart = function () {
+const cartModal = function () {
 	const cartBtn = document.querySelector('.button-cart'),
 		cart = document.getElementById('modal-cart'),
 		closeBtn = cart.querySelector('.modal-close');
 
+	// cartBtn.addEventListener('click', function () {
+	// 	cart.style.display = 'flex';
+	// });
+
+	// closeBtn.addEventListener('click', function () {
+	// 	cart.style.display = '';
+	// });
+	// ================== мой вариант ======================
 	cartBtn.addEventListener('click', function () {
-		cart.style.display = 'flex';
+		cart.classList.add('show');
+	});
+	closeBtn.addEventListener('click', function () {
+		cart.classList.remove('show');
 	});
 
-	closeBtn.addEventListener('click', function () {
-		cart.style.display = '';
-	});
 }
-cart();
+cartModal();
